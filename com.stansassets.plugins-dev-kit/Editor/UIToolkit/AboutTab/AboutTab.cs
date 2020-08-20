d8b0cb6afb452d585f710754efa4082f03adb2a8
Rufield <biglazycatukr@gmail.com>
2020-08-21 01:32:39 +0300

    update aboutTab

--- com.stansassets.plugins-dev-kit/Editor/UIToolkit/AboutTab/AboutTab.cs
+++ com.stansassets.plugins-dev-kit/Editor/UIToolkit/AboutTab/AboutTab.cs
@@ -1,11 +1,14 @@
-﻿namespace StansAssets.Plugins.Editor
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace StansAssets.Plugins.Editor
 {
     public class AboutTab : BaseTab
     {
         public AboutTab()
             : base($"{PluginsDevKitPackage.UIToolkitPath}/AboutTab/AboutTab")
         {
-
+           // this.Q<Button>("GamesButton").clicked += () => { Application.OpenURL("https://stansassets.com/#portfolio"); };
         }
     }
 }

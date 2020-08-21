f14b12c8c05c1b4d369aa601db8d5fa09db8f12c
Rufield <biglazycatukr@gmail.com>
2020-08-21 20:06:42 +0300

    added SetButtonHyperlink metod

--- com.stansassets.plugins-dev-kit/Editor/UIToolkit/AboutTab/AboutTab.cs
+++ com.stansassets.plugins-dev-kit/Editor/UIToolkit/AboutTab/AboutTab.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Windows.Markup;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace StansAssets.Plugins.Editor
@@ -8,7 +9,29 @@ namespace StansAssets.Plugins.Editor
         public AboutTab()
             : base($"{PluginsDevKitPackage.UIToolkitPath}/AboutTab/AboutTab")
         {
-           // this.Q<Button>("GamesButton").clicked += () => { Application.OpenURL("https://stansassets.com/#portfolio"); };
+            SetButtonHyperlink("GamesElement", "https://stansassets.com/#portfolio");
+            SetButtonHyperlink("PluginsElement", "https://assetstore.unity.com/publishers/2256");
+            SetButtonHyperlink("TeamElement", "https://stansassets.com/#our-team");
+
+            SetButtonHyperlink("supportEmail", "mailto:support@stansassets.com");
+            SetButtonHyperlink("ceoEmail", "mailto:ceo@stansassets.com");
+
+            SetButtonHyperlink("LinkedinElement", "https://www.linkedin.com/in/lacost");
+            SetButtonHyperlink("TwitterElement", "https://twitter.com/stansassets");
+            SetButtonHyperlink("FacebookElement", "https://www.facebook.com/stansassets/");
+
+            SetButtonHyperlink("YoutubeElement", "https://www.youtube.com/user/stansassets/videos");
+            SetButtonHyperlink("GooglePlusElement", "https://plus.google.com/+StansassetsOfficia");
+            SetButtonHyperlink("TwitchElement", "https://www.twitch.tv/stans_assets");
+
+            SetButtonHyperlink("LogoElement", "https://stansassets.com/");
+        }
+
+        void SetButtonHyperlink(string btnName, string url)
+        {
+            var btn = this.Q<Button>(btnName);
+            btn.clicked += () =>
+                Application.OpenURL(url);
         }
     }
 }

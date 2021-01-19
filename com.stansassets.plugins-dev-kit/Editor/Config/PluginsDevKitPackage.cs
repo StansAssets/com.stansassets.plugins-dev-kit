72a79088e074a6e57bd0a65b629bcf7911383897
Stanislav Osipov <lacost.st@gmail.com>
2021-01-19 19:28:17 +0200

    chore: dev kit editor is now auto referenced.

--- com.stansassets.plugins-dev-kit/Editor/Config/PluginsDevKitPackage.cs
+++ com.stansassets.plugins-dev-kit/Editor/Config/PluginsDevKitPackage.cs
@@ -12,15 +12,16 @@ namespace StansAssets.Plugins.Editor
         /// Package runtime settings location path.
         /// </summary>
         public const string RootMenu = "Stan's Assets";
+        public const string ProductivityRootMenu = RootMenu + "/Productivity";
         public const string StansAssetsSupportEmail = "support@stansassets.com";
         public const string StansAssetsCeoEMail = "ceo@stansassets.com";
         public const string StansAssetsWebsiteRootUrl = "https://stansassets.com/";
-        
+
         public const string Name = "com.stansassets.plugins-dev-kit";
         public static readonly string RootPath = PackageManagerUtility.GetPackageRootPath(Name);
         public static readonly string UIToolkitPath = $"{RootPath}/Editor/UIToolkit";
         public static readonly string UIToolkitControlsPath = $"{UIToolkitPath}/Controls";
-        
+
         public static readonly string EditorArtAssetsPath = $"{RootPath}/Editor/Art";
         public static readonly string EditorIconAssetsPath = $"{EditorArtAssetsPath}/Icons";
         public static readonly string EditorFontAssetsPath = $"{EditorArtAssetsPath}/Fonts";

704154f11fe1518b88340e8dfbfa0840025f077b
Danylo Lytvynenko <biglazycatukr@gmail.com>
2021-03-04 14:49:35 +0200

    feat: LocalProjectSettingsSingleton

---
+++ com.stansassets.plugins-dev-kit/Runtime/PackageSettings/LocalProjectSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace StansAssets.Plugins
+{
+    [Serializable]
+    public class LocalProjectSettings
+    {
+        public LocalProjectSettings()
+        {
+            SettingsFileName = GetType().Name;
+            PackageName = "newPackage";
+        }
+        
+        public LocalProjectSettings(string packageName, string settingsFileName)
+        {
+            SettingsFileName = settingsFileName;
+            PackageName = packageName;
+        }
+
+        /// <summary>
+        /// Plugin package name.
+        /// </summary>
+        public string PackageName { get; }
+
+        /// <summary>
+        /// Plugin settings folder path.
+        /// </summary>
+        public string SettingsFolderPath => $"{Application.dataPath}/{PackagesConfig.LibraryPath}/{PackageName}";
+
+        /// <summary>
+        /// Plugin settings file path.
+        /// </summary>
+        public string SettingsFilePath => $"{SettingsFolderPath}/{SettingsFileName}.json";
+
+        /// <summary>
+        /// Settings file name.
+        /// </summary>
+        public string SettingsFileName { get; }
+    }
+}

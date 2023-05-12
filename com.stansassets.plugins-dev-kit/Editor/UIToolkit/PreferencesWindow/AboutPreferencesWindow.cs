ae8b19bb292033922c374ead517ef0be538955b8
Yurii Lunha <yurii.lunha@stansassets.com>
2023-05-12 08:59:27 +0200

    refactor: restructured and updated registration process of PackagePreferencesWindow

--- com.stansassets.plugins-dev-kit/Editor/UIToolkit/PreferencesWindow/AboutPreferencesWindow.cs
+++ com.stansassets.plugins-dev-kit/Editor/UIToolkit/PreferencesWindow/AboutPreferencesWindow.cs
@@ -1,6 +1,6 @@
 ﻿#if UNITY_2019_4_OR_NEWER
 
-using System.Collections.Generic;
+using JetBrains.Annotations;
 using StansAssets.Foundation.Editor;
 using UnityEditor;
 using UnityEngine.UIElements;
@@ -8,39 +8,25 @@ using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace StansAssets.Plugins.Editor
 {
+    [UsedImplicitly]
     sealed class AboutPreferencesWindow : PackagePreferencesWindow
     {
-        AboutPreferencesWindow(string path, SettingsScope scopes, IEnumerable<string> keywords = null)
-            : base(path, scopes, keywords)
-        {
-        }
-
         protected override PackageInfo GetPackageInfo()
+            => PackageManagerUtility.GetPackageInfo(PluginsDevKitPackage.Name);
+
+        protected override string DisplayName => "Plugin Dev Kit";
+        protected override string SettingsPath => $"{PluginsDevKitPackage.RootMenu}/{DisplayName}";
+        protected override SettingsScope Scope => SettingsScope.User;
+
+        protected override void OnActivate(string searchContext, VisualElement rootElement)
         {
-            return PackageManagerUtility.GetPackageInfo(PluginsDevKitPackage.Name);
+            ContentContainerFlexGrow(1);
+            AddTab("About", new AboutTab());
+            ActivateTab("About");
         }
 
-        public override void OnActivate(string searchContext, VisualElement rootElement)
+        protected override void OnDeactivate()
         {
-            base.OnActivate(searchContext, rootElement);
-
-            TabController.ContentContainerFlexGrow(1);
-            TabController.AddTab("about", "About", new AboutTab());
-            TabController.ActivateTab("about");
-        }
-
-        [SettingsProvider]
-        static SettingsProvider RegisterSettingsProvider()
-        {
-            var provider = new AboutPreferencesWindow(
-                $"{PluginsDevKitPackage.RootMenu}/Plugin Dev Kit",
-                SettingsScope.User,
-                new[] { "stan", "plugin", "dev", "kit" })
-            {
-                label = "About"
-            };
-
-            return provider;
         }
     }
 }
--- com.stansassets.plugins-dev-kit/Editor/UIToolkit/PreferencesWindow/PackagePreferencesWindow.cs
+++ com.stansassets.plugins-dev-kit/Editor/UIToolkit/PreferencesWindow/PackagePreferencesWindow.cs
@@ -1,6 +1,8 @@
 ﻿#if UNITY_2019_4_OR_NEWER
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using StansAssets.Foundation.Editor;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -10,11 +12,74 @@ namespace StansAssets.Plugins.Editor
 {
     /// <summary>
     /// Base class for Plugin Preferences Window
-    /// See <see cref="AboutPreferencesWindow"/> as an example for registering window in preferences (user, project)
     /// </summary>
-    public abstract class PackagePreferencesWindow : SettingsProvider
+    public abstract class PackagePreferencesWindow
     {
-        public override void OnActivate(string searchContext, VisualElement rootElement)
+        TabController m_TabController;
+
+        /// <summary>
+        /// Structure describing a Unity Package.
+        /// </summary>
+        protected abstract UnityEditor.PackageManager.PackageInfo GetPackageInfo();
+
+        /// <summary>
+        /// Gets or sets the display name of the SettingsProvider as it appears in the Settings window.
+        /// If not set, the Settings window uses last token of SettingsProvider.settingsPath instead.
+        /// </summary>
+        protected abstract string DisplayName { get; }
+
+        /// <summary>
+        /// Gets Path used to place the SettingsProvider in the tree view of the Settings window.
+        /// The path should be unique among all other settings paths and should use "/" as its separator.
+        /// </summary>
+        protected abstract string SettingsPath { get; }
+
+        /// <summary>
+        /// Gets the Scope of the SettingsProvider. The Scope determines whether the SettingsProvider appears
+        /// in the Preferences window (SettingsScope.User) or the Settings window (SettingsScope.Project).
+        /// </summary>
+        protected abstract SettingsScope Scope { get; }
+
+        /// <summary>
+        /// Add tab to the window top bar.
+        /// </summary>
+        /// <param name="label">Tab label.</param>
+        /// <param name="content">Tab content.</param>
+        /// <exception cref="ArgumentException">Will throw tab with the same label was already added.</exception>
+        protected void AddTab(string label, VisualElement content)
+        {
+            m_TabController.AddTab(label, content);
+        }
+
+        /// <summary>
+        /// Activate tab by name
+        /// </summary>
+        /// <param name="name">Early specified tab name</param>
+        protected void ActivateTab(string name)
+        {
+            m_TabController.ActivateTab(name);
+        }
+
+        /// <summary>
+        /// Set the flexible growth property of tabs content container
+        /// </summary>
+        /// <param name="styleFloat"></param>
+        protected void ContentContainerFlexGrow(StyleFloat styleFloat)
+        {
+            m_TabController.ContentContainerFlexGrow(styleFloat);
+        }
+
+        /// <summary>
+        /// Overrides SettingsProvider.OnActivate.
+        /// </summary>
+        protected abstract void OnActivate(string searchContext, VisualElement rootElement);
+
+        /// <summary>
+        /// Overrides SettingsProvider.OnDeactivate.
+        /// </summary>
+        protected abstract void OnDeactivate();
+        
+        void OnActivateWindow(string searchContext, VisualElement rootElement)
         {
             UIToolkitEditorUtility.CloneTreeAndApplyStyle(rootElement,
                 $"{PluginsDevKitPackage.UIToolkitPath}/SettingsWindow/PackageSettingsWindow");
@@ -27,21 +92,51 @@ namespace StansAssets.Plugins.Editor
                 searchBar.style.visibility = Visibility.Hidden;
             }
 
+            rootElement.Q<Label>("display-name").text = DisplayName;
+
             var packageInfo = GetPackageInfo();
-            rootElement.Q<Label>("display-name").text = packageInfo.displayName.Remove(0, "Stans Assets - ".Length);
             rootElement.Q<Label>("description").text = packageInfo.description;
             rootElement.Q<Label>("version").text = $"Version: {packageInfo.version}";
 
-            TabController = new TabController(rootElement);
+            m_TabController = new TabController(rootElement);
         }
 
-        protected TabController TabController { get; private set; }
-
-        protected abstract UnityEditor.PackageManager.PackageInfo GetPackageInfo();
-
-        protected PackagePreferencesWindow(string path, SettingsScope scopes, IEnumerable<string> keywords = null)
-            : base(path, scopes, keywords)
+        SettingsProvider ConstructSettingsProvider()
         {
+            var packageInfo = GetPackageInfo();
+            var settingsProvider = new SettingsProvider(SettingsPath, Scope, packageInfo.keywords)
+            {
+                label = DisplayName,
+            };
+
+            settingsProvider.activateHandler += OnActivateWindow;
+            settingsProvider.activateHandler += OnActivate;
+            settingsProvider.deactivateHandler += OnDeactivate;
+
+            return settingsProvider;
+        }
+
+        [SettingsProviderGroup]
+        static SettingsProvider[] RegisterSettingsProviderGroup()
+        {
+            var baseType = typeof(PackagePreferencesWindow);
+            var group = new List<SettingsProvider>();
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var derivedTypes = assembly.GetTypes()
+                    .Where(t => baseType.IsAssignableFrom(t) && t != baseType)
+                    .ToArray();
+
+                foreach (var derivedType in derivedTypes)
+                {
+                    var instance = (PackagePreferencesWindow)Activator.CreateInstance(derivedType);
+                    var settingsProvider = instance.ConstructSettingsProvider();
+                    group.Add(settingsProvider);
+                }
+            }
+
+            return group.ToArray();
         }
     }
 }
